8d1c125e48bba3e571eca54a30c23a0a4395940d
jhonMarques <johnatasbcc@gmail.com>
2021-06-26 20:50:22 -0300

    Perfil de Dev implementado

---
+++ src/main/java/com/jhonatas/bookstore/config/DevConfig.java
@@ -0,0 +1,29 @@
+package com.jhonatas.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.jhonatas.bookstore.service.DBservice;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBservice dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instanciaBaseDeDados() {
+		if(strategy.equals("create")) {
+			this.dbService.instanciaBaseDeDados();
+		}
+		return false;
+	}
+
+}
