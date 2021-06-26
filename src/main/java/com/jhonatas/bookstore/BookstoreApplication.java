0c93c0d1cbc0536d3ceccfa26679a30d67226fa7
jhonMarques <johnatasbcc@gmail.com>
2021-06-26 18:27:30 -0300

    Teste implementado

--- src/main/java/com/jhonatas/bookstore/BookstoreApplication.java
+++ src/main/java/com/jhonatas/bookstore/BookstoreApplication.java
@@ -1,41 +1,19 @@
 package com.jhonatas.bookstore;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.jhonatas.bookstore.domain.Categoria;
-import com.jhonatas.bookstore.domain.Livro;
-import com.jhonatas.bookstore.repositories.CategoriaRepository;
-import com.jhonatas.bookstore.repositories.LivroRepository;
+import com.jhonatas.bookstore.service.DBservice;
 
 @SpringBootApplication
-public class BookstoreApplication implements CommandLineRunner{
+public class BookstoreApplication{
 	
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	@Autowired
-	private LivroRepository livroRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
 		
-		Categoria categoria = new Categoria(null, "Informatica", "Livro de TI");
-		
-		Livro livro = new Livro(null, "Clean Code", "Robert Martin", "Lorem ipsum", categoria);
-		
-		categoria.getLivros().addAll(Arrays.asList(livro));	
-		
-		this.categoriaRepository.saveAll(Arrays.asList(categoria));
-		this.livroRepository.saveAll(Arrays.asList(livro));
 	}
 	
-
-}
+		
+	}
+	
---
+++ src/main/java/com/jhonatas/bookstore/config/TesteConfig.java
@@ -0,0 +1,22 @@
+package com.jhonatas.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.jhonatas.bookstore.service.DBservice;
+
+@Configuration
+@Profile("test")
+public class TesteConfig {
+	
+	@Autowired
+	private DBservice dbService;
+	
+	@Bean
+	public void instanciaBaseDeDados() {
+		this.dbService.instanciaBaseDeDados();
+	}
+
+}
