8f68fd1744389d75c559d3df3c3f410c1307bb3c
JhonMarques <jhonatasbcc@gmail.com>
2021-07-10 19:52:09 -0300

    Listando todos os livros por categoria

--- src/main/java/com/jhonatas/bookstore/repositories/LivroRepository.java
+++ src/main/java/com/jhonatas/bookstore/repositories/LivroRepository.java
@@ -2,10 +2,16 @@ package com.jhonatas.bookstore.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.jhonatas.bookstore.domain.Livro;
+
+import java.util.List;
 
 @Repository
 public interface LivroRepository extends JpaRepository<Livro, Integer>{
 
+    @Query("SELECT obj FROM Livro obj WHERE obj.categoria.id = :id_cat ORDER BY titulo")
+    List<Livro> findAllByCategoria(@Param(value = "id_cat") Integer id_cat);
 }
--- src/main/java/com/jhonatas/bookstore/resources/LivroResources.java
+++ src/main/java/com/jhonatas/bookstore/resources/LivroResources.java
@@ -1,10 +1,14 @@
 package com.jhonatas.bookstore.resources;
 
 import com.jhonatas.bookstore.domain.Livro;
+import com.jhonatas.bookstore.dtos.LivroDTO;
 import com.jhonatas.bookstore.service.LivroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/livros")
@@ -18,5 +22,17 @@ public class LivroResources {
 
         Livro obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    //Busca livros apresentando apenas Livro.Id, Livro.Titulo, Id.Categoria e Titulo.categoria
+    @GetMapping
+    public ResponseEntity<List<LivroDTO>> findAll(@RequestParam (value = "categoria",
+        defaultValue = "0") Integer id_cat){
+        List<Livro> livros = service.findAll(id_cat);
+        List<LivroDTO> listDTO = livros.stream()
+            .map(obj -> new LivroDTO(obj))
+            .collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
+        //localhost:8080/livrod?categoria=1
     }
 }
